9620271a91c274d12af8ef59cff49aeff2b05c77
ArchWizard56 <stephen56swnsn@gmail.com>
2017-10-17 19:37:03 -0700

    Renamed things for clarity

--- sphereHandler.js
+++ sphereHandler.js
@@ -1,6 +1,5 @@
 AFRAME.registerComponent('hello-world', {
   init: function () {
-    console.log('Hello, World!');
     var el = this.el;
     el.addEventListener('click', function () {
     el.setAttribute('position', {x: 1, y: 2, z: -3});
