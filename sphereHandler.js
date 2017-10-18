d7c9189a21303537a8cfa427f2ec6bc511515727
ArchWizard56 <stephen56swnsn@gmail.com>
2017-10-17 20:45:00 -0700

    Fixed if\else bugs

--- sphereHandler.js
+++ sphereHandler.js
@@ -2,10 +2,12 @@ AFRAME.registerComponent('sphere-handler', {
   init: function () {
     var el = this.el;
     var isAtStart = true;
+    var anim
+    var anim2
     el.addEventListener('click', function () { // Create the Listener for the gaze
-    var anim = document.createElement("a-animation"); // Create a new animation
-    var anim2 = document.createElement("a-animation"); // Create a new animation
     if (isAtStart == true) {
+        anim = document.createElement("a-animation"); // Create a new animation
+        anim2 = document.createElement("a-animation"); // Create a new animation
         anim.setAttribute("attribute", "position"); // Set what the animation is animating
         anim.setAttribute("to", "-6 10 2"); // Set the end value
         anim.setAttribute("from", "1.8 .5 0"); // Set the end value
@@ -16,9 +18,12 @@ AFRAME.registerComponent('sphere-handler', {
         anim2.setAttribute("from", "green"); // Set the beginning value
         anim2.setAttribute("dur", "10000"); // Set the duration
         anim2.setAttribute("repeat", "0"); // Set the repeat
-    var isAtStart = false;
-    }
-    else {  
+        isAtStart = false;
+        el.appendChild(anim); // Add the animation to the sphere
+        el.appendChild(anim2); // Add the animation to the sphere
+    } else if (isAtStart == false) {  
+        anim = document.createElement("a-animation"); // Create a new animation
+        anim2 = document.createElement("a-animation"); // Create a new animation
         anim.setAttribute("attribute", "position"); // Set what the animation is animating
         anim.setAttribute("to", "1.8 .5 0"); // Set the end value
         anim.setAttribute("from", "-6 10 2"); // Set the end value
@@ -29,10 +34,10 @@ AFRAME.registerComponent('sphere-handler', {
         anim2.setAttribute("from", "red"); // Set the beginning value
         anim2.setAttribute("dur", "10000"); // Set the duration
         anim2.setAttribute("repeat", "0"); // Set the repeat
-    var isAtStart = true;
+        isAtStart = true;
+        el.appendChild(anim); // Add the animation to the sphere
+        el.appendChild(anim2); // Add the animation to the sphere
     }
-    el.appendChild(anim); // Add the animation to the sphere
-    el.appendChild(anim2); // Add the animation to the sphere
     }.bind(this))
   }
 });
